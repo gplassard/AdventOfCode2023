884ec9b8de9c7022e18a1e8ac2dee2775576a658
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-03 11:33:42 +0100

    feat(day3): part 2

--- src/main/scala/day3/Day3.scala
+++ src/main/scala/day3/Day3.scala
@@ -28,22 +28,57 @@ object Day3 {
     }
     sum
 
-  def hasPartNeighbor(col: Int, row: Int, engineMap2D: List[List[Char]], width: Int, height: Int): Boolean = {
-    val neighbors = List(
-      (Math.max(col - 1, 0), Math.max(row - 1, 0)),
-      (Math.max(col - 1, 0), Math.max(row - 0, 0)),
-      (Math.max(col - 1, 0), Math.min(row + 1, height - 1)),
-      (Math.max(col - 0, 0), Math.max(row - 1, 0)),
-      (Math.max(col - 0, 0), Math.max(row - 0, 0)),
-      (Math.max(col - 0, 0), Math.min(row + 1, height - 1)),
-      (Math.min(col + 1, width - 1), Math.max(row - 1, 0)),
-      (Math.min(col + 1, width - 1), Math.max(row - 0, 0)),
-      (Math.min(col + 1, width - 1), Math.min(row + 1, height - 1)),
-    )
-    val res = neighbors.exists((col, row) => isPart(engineMap2D(row)(col)))
-    res
-  }
+  def hasPartNeighbor(col: Int, row: Int, engineMap2D: List[List[Char]], width: Int, height: Int): Boolean =
+    neighbors(col, row, engineMap2D, width, height).exists((col, row) => isPart(engineMap2D(row)(col)))
+
+  def neighbors(col: Int, row: Int, engineMap2D: List[List[Char]], width: Int, height: Int): List[(Int, Int)] = List(
+    (Math.max(col - 1, 0), Math.max(row - 1, 0)),
+    (Math.max(col - 1, 0), Math.max(row - 0, 0)),
+    (Math.max(col - 1, 0), Math.min(row + 1, height - 1)),
+    (Math.max(col - 0, 0), Math.max(row - 1, 0)),
+    (Math.max(col - 0, 0), Math.max(row - 0, 0)),
+    (Math.max(col - 0, 0), Math.min(row + 1, height - 1)),
+    (Math.min(col + 1, width - 1), Math.max(row - 1, 0)),
+    (Math.min(col + 1, width - 1), Math.max(row - 0, 0)),
+    (Math.min(col + 1, width - 1), Math.min(row + 1, height - 1)),
+  )
 
   def isPart(c: Char): Boolean = c != '.' && !c.isDigit
+
+
+  def part2(lines: List[String]): Int =
+    val engineMap2D = lines.map(line => line.toCharArray.toList)
+    val width = engineMap2D.headOption.map(_.length).getOrElse(0)
+    val height = engineMap2D.length
+    var gearCandidates: Map[(Int, Int), List[Int]] = Map.empty
+    var currentPartNumber = ""
+    var starNeighbors: Set[(Int, Int)] = Set.empty
+    for (row <- 0 until height) {
+      for (col <- 0 until width) {
+        val element = engineMap2D(row)(col)
+        if (element.isDigit) {
+          starNeighbors = starNeighbors ++ getStarNeighborsPositions(col, row, engineMap2D, width, height)
+          currentPartNumber = currentPartNumber + element
+        }
+        else if (currentPartNumber.nonEmpty && starNeighbors.nonEmpty) {
+          for (neighbor <- starNeighbors) {
+            val updatedGear = currentPartNumber.toInt :: gearCandidates.getOrElse(neighbor, List.empty)
+            gearCandidates = gearCandidates + (neighbor -> updatedGear)
+          }
+        }
+        if (!element.isDigit) {
+          currentPartNumber = ""
+          starNeighbors = Set.empty
+        }
+      }
+    }
+    gearCandidates
+      .values
+      .filter(_.length == 2)
+      .map(_.product)
+      .sum
+
+  def getStarNeighborsPositions(col: Int, row: Int, engineMap2D: List[List[Char]], width: Int, height: Int): List[(Int, Int)] =
+    neighbors(col, row, engineMap2D, width, height).filter((col, row) => engineMap2D(row)(col) == '*')
 
 }
--- src/test/scala/day3/Day3Test.scala
+++ src/test/scala/day3/Day3Test.scala
@@ -34,4 +34,28 @@ class Day3Test extends AnyWordSpec with Matchers {
     }
   }
 
+  "part2" should {
+    "be 0 for no lines" in {
+      Day3.part2(List.empty) should equal(0)
+    }
+
+    "count if there is a gear" in {
+      Day3.part2(List("10*11...13")) should equal(110)
+    }
+
+    "not count if there are no gears" in {
+      Day3.part2(List("467*.114..")) should equal(0)
+    }
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day3/sample.txt")).toScala(List)
+      Day3.part2(measures) should equal(467835)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day3/input.txt")).toScala(List)
+      Day3.part2(measures) should equal(83279367)
+    }
+  }
+
 }
