c69d01be627abac928b99907ee00a40f94426955
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-12-04 20:29:07 +0100

    feat(day4): part2 (#2)
    * tmp
    
    * feat(day4): part2

--- src/main/scala/day4/Day4.scala
+++ src/main/scala/day4/Day4.scala
@@ -12,4 +12,28 @@ object Day4 {
       Math.pow(2, intersect.size - 1).toInt
     }.sum
 
+  case class Card(id: Int, rewards: Set[Int])
+
+  def part2(lines: List[String]): Int = {
+    val cards = winningCombos(lines)
+    var cardsCount = cards.map(_ => 1)
+    cards.foreach(card => {
+      val count = cardsCount(card.id - 1)
+      card.rewards.map { reward =>
+        val updatedValue = cardsCount(reward - 1) + count
+        cardsCount = cardsCount.updated(reward - 1, updatedValue)
+      }
+    })
+    cardsCount.sum
+  }
+
+  def winningCombos(lines: List[String]): List[Card] = lines.map { line =>
+    val id = line.split(":")(0).replace("Card", "").trim.toInt
+    val lists = line.split(":")(1).split("\\|")
+    val winning = lists(0).split(" ").map(_.trim).filter(_.nonEmpty).map(_.toInt).toSet
+    val cards = lists(1).split(" ").map(_.trim).filter(_.nonEmpty).map(_.toInt).toSet
+    val intersect = winning.intersect(cards)
+    Card(id, (1 + id to (id + intersect.size)).toSet)
+  }
+
 }
--- src/test/scala/day4/Day4Test.scala
+++ src/test/scala/day4/Day4Test.scala
@@ -33,4 +33,34 @@ class Day4Test extends AnyWordSpec with Matchers {
       Day4.part1(measures) should equal(27845)
     }
   }
+
+  "part2" should {
+    "be 0 for no lines" in {
+      Day4.part2(List.empty) should equal(0)
+    }
+
+    "count if there are winning cards" in {
+      Day4.part2(List(
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 41 | 42",
+        "Card 3: 41 | 42",
+        "Card 4: 41 | 42",
+        "Card 5: 41 | 42",
+      )) should equal(9)
+    }
+
+    "be the number of cards if there are no winning cards" in {
+      Day4.part2(List("Card 1: 1 2 3 | 4 5 6")) should equal(1)
+    }
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day4/sample.txt")).toScala(List)
+      Day4.part2(measures) should equal(30)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day4/input.txt")).toScala(List)
+      Day4.part2(measures) should equal(9496801)
+    }
+  }
 }
