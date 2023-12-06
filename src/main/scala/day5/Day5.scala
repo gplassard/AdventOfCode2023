75edd46716f290f97247a90e36f8e9f6d286c801
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-06 21:49:23 +0100

    feat(day5): part2

--- src/main/scala/day5/Day5.scala
+++ src/main/scala/day5/Day5.scala
@@ -1,6 +1,8 @@
 package fr.gplassard.adventofcode
 package day5
 
+import scala.collection.mutable.Stack as MStack
+
 object Day5 {
 
   object Transformer {
@@ -33,4 +35,54 @@ object Day5 {
     numbers = numbers.map(n => transformers.map(t => t.transform(n)).find(_.isDefined).map(_.getOrElse(n)).getOrElse(n))
     numbers.minOption.getOrElse(-1)
 
+  case class Range(start: Long, end: Long) {
+    def isValid(): Boolean = start <= end
+    def transform(transformer: Transformer): (Option[Range], Option[Range], Option[Range]) = {
+      val tStart = transformer.source
+      val tEnd = transformer.source + transformer.length
+      val delta = transformer.destination - transformer.source
+      val intersectionRange = Some(Range(Math.max(start, tStart), Math.min(end, tEnd))).filter(_.isValid())
+      val belowRange = Some(Range(start, Math.min(end, tStart - 1))).filter(_.isValid())
+      val overRange = Some(Range(Math.max(start, tEnd + 1), end)).filter(_.isValid())
+      (
+        intersectionRange.map(r => Range(r.start  + delta, r.end + delta)),
+        belowRange,
+        overRange
+      )
+    }
+  }
+
+  def part2(lines: List[String]): Long =
+    val numbers = lines(0).replace("seeds:", "").split(" ").map(_.trim).filter(_.nonEmpty).map(_.toLong)
+    var seeds = MStack.from(numbers.grouped(2).map{case Array(start, length) => Range(start, start + length - 1)}.toList)
+    var transformers = List.empty[Transformer]
+    for (line <- (lines appended "")) {
+      if (line.isBlank && transformers.nonEmpty) {
+        val newSeeds = MStack.empty[Range]
+        while (seeds.nonEmpty) {
+          val seed = seeds.pop()
+          var added = false
+          for (transformer <- transformers) {
+            val (transformed, lower, higher) = seed.transform(transformer)
+            if (transformed.nonEmpty) {
+              transformed.foreach({ transformed =>
+                added = true
+                newSeeds.append(transformed)
+              })
+              lower.foreach(seeds.append)
+              higher.foreach(seeds.append)
+            }
+          }
+          if (!added) {
+            newSeeds.append(seed)
+          }
+        }
+        seeds = newSeeds
+        transformers = List.empty
+      }
+      else if (!line.contains(":") && !line.contains("seeds") && !line.isBlank) {
+        transformers = transformers :+ Transformer.parse(line)
+      }
+    }
+    seeds.map(_.start).minOption.getOrElse(-1)
 }
--- src/test/scala/day5/Day5Test.scala
+++ src/test/scala/day5/Day5Test.scala
@@ -47,4 +47,92 @@ class Day5Test extends AnyWordSpec with Matchers {
       Day5.part1(measures) should equal(525792406)
     }
   }
+
+  "Range" should {
+    "not intersect with a transformer (lower)" in {
+      // source             0...10
+      // transformer from   ...........100...110
+      // transformer to     .........................200...210
+      // result             0...10
+
+      val (intersected, lower, higher) = Day5.Range(0, 10).transform(Day5.Transformer(200, 100, 10))
+
+      intersected should equal(None)
+      lower should equal(Some(Day5.Range(0, 10)))
+      higher should equal(None)
+    }
+
+    "not intersect with a transformer (higher)" in {
+      // source             .....................................300...310
+      // transformer from   ...........100...110
+      // transformer to     .........................200...210
+      // result             .....................................300...310
+      val (intersected, lower, higher) = Day5.Range(300, 310).transform(Day5.Transformer(200, 100, 10))
+
+      intersected should equal(None)
+      lower should equal(None)
+      higher should equal(Some(Day5.Range(300, 310)))
+    }
+
+    "intersect in lower" in {
+      // source             .........95.....105
+      // transformer from   ............100.....110
+      // transformer to     .........................200.....210
+      // result             .........95.99...........200.205
+      val (intersected, lower, higher) = Day5.Range(95, 105).transform(Day5.Transformer(200, 100, 10))
+
+      intersected should equal(Some(Day5.Range(200, 205)))
+      lower should equal(Some(Day5.Range(95, 99)))
+      higher should equal(None)
+    }
+
+    "intersect in higher" in {
+      // source             ................105..........115
+      // transformer from   ............100.....110
+      // transformer to     ......................................200.....210
+      // result             ........................111...115.........205.210
+      val (intersected, lower, higher) = Day5.Range(105, 115).transform(Day5.Transformer(200, 100, 10))
+
+      intersected should equal(Some(Day5.Range(205, 210)))
+      lower should equal(None)
+      higher should equal(Some(Day5.Range(111, 115)))
+    }
+
+    "overlapping source" in {
+      // source             .......95....................115
+      // transformer from   ............100.....110
+      // transformer to     ...................................200.....210
+      // result             .......95.99............111..115...200.....210
+      val (intersected, lower, higher) = Day5.Range(95, 115).transform(Day5.Transformer(200, 100, 10))
+
+      intersected should equal(Some(Day5.Range(200, 210)))
+      lower should equal(Some(Day5.Range(95, 99)))
+      higher should equal(Some(Day5.Range(111, 115)))
+    }
+
+    "overlapping transformer" in {
+      // source             ............100.....110
+      // transformer from   .......95.................115
+      // transformer to     ...................................195.............215
+      // result             ........................................200..210
+      val (intersected, lower, higher) = Day5.Range(100, 110).transform(Day5.Transformer(195, 95, 20))
+
+      intersected should equal(Some(Day5.Range(200, 210)))
+      lower should equal(None)
+      higher should equal(None)
+    }
+  }
+
+  "part2" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day5/sample.txt")).toScala(List)
+      Day5.part2(measures) should equal(46)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day5/input.txt")).toScala(List)
+      Day5.part2(measures) should equal(79004094)
+    }
+  }
 }
