631986786a11095b68f7a7ff4ef25f2b5236637a
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-07 23:56:57 +0100

    feat(day7): part2

--- src/main/scala/day07/Day07.scala
+++ src/main/scala/day07/Day07.scala
@@ -2,17 +2,33 @@ package fr.gplassard.adventofcode
 package day07
 
 object Day07 {
-  private val cardRank: Seq[Char] = List('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
-
   enum HandType {
     case FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind, TwoPairs, OnePair, HighCard
   }
 
-  implicit object HandOrdering extends Ordering[Hand] {
-    def compare(a: Hand, b: Hand): Int = a.compareTo(b)
+  implicit object HandPart1Ordering extends Ordering[HandPart1] {
+    private val cardRank: Seq[Char] = List('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+    def compare(a: HandPart1, b: HandPart1): Int = a.compareTo(b, cardRank)
   }
 
-  case class Hand(cards: List[Char]) {
+  trait Hand {
+    val cards: List[Char]
+    def handType(): HandType
+    def compareTo(other: Hand, cardRank: Seq[Char]): Int = {
+      val handRankDiff = this.handType().ordinal - other.handType().ordinal
+      if (handRankDiff != 0) handRankDiff
+      else {
+        this.cards.map(cardRank.indexOf(_))
+          .zip(other.cards.map(cardRank.indexOf(_)))
+          .map(x => x._1 - x._2)
+          .find(_ != 0)
+          .getOrElse(0)
+      }
+    }
+  }
+
+
+  case class HandPart1(cards: List[Char]) extends Hand {
     def handType(): HandType = {
       cards.groupBy(identity).view.mapValues(_.size) match {
         case m if m.values.max == 5 => HandType.FiveOfAKind
@@ -24,28 +40,18 @@ object Day07 {
         case _ => HandType.HighCard
       }
     }
-
-    def compareTo(other: Hand): Int = {
-      val handRankDiff = this.handType().ordinal - other.handType().ordinal
-      if (handRankDiff != 0) handRankDiff
-      else {
-        this.cards.map(cardRank.indexOf(_))
-          .zip(other.cards.map(cardRank.indexOf(_)))
-          .map(x => x._1 - x._2)
-          .find(_ != 0)
-          .getOrElse(0)
-      }
-    }
-
   }
 
-  def part1(lines: List[String]): Int = {
-    lines
+  def part1(lines: List[String]): Int =
+    solve(lines
       .map { line =>
-        val hand = Hand(line.split(" ")(0).toList)
+        val hand = HandPart1(line.split(" ")(0).toList)
         val bid = line.split(" ")(1).toInt
         (hand, bid)
-      }
+      })
+
+  private def solve[T](parsedLines: List[(T, Int)])(implicit ordering: Ordering[T]): Int = {
+    parsedLines
       .sortBy((hand, _) => hand)
       .reverse
       .zipWithIndex
@@ -53,7 +59,33 @@ object Day07 {
       .sum
   }
 
-  def part2(lines: List[String]): Int = {
-    -1
+
+  implicit object HandPart2Ordering extends Ordering[HandPart2] {
+    private val cardRank: Seq[Char] = List('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
+    def compare(a: HandPart2, b: HandPart2): Int = a.compareTo(b, cardRank)
   }
+
+  case class HandPart2(cards: List[Char]) extends Hand {
+    def handType(): HandType = {
+      val grouped = cards.groupBy(identity)
+      val jokerCount = grouped.getOrElse('J', List.empty).size
+      val differentCards = grouped.size - (if (jokerCount > 0) 1 else 0)
+      grouped.filter(_._1 != 'J').view.mapValues(_.size) match {
+        case m if (m.values.maxOption.getOrElse(0) + jokerCount) == 5 => HandType.FiveOfAKind
+        case m if (m.values.max + jokerCount) == 4 => HandType.FourOfAKind
+        case m if differentCards == 2 && (m.values.max + jokerCount) == 3 => HandType.FullHouse
+        case m if (m.values.max + jokerCount) == 3 => HandType.ThreeOfAKind
+        case m if m.size == 3 && m.values.max == 2 => HandType.TwoPairs
+        case m if (m.values.max + jokerCount) == 2 => HandType.OnePair
+        case _ => HandType.HighCard
+      }
+    }
+  }
+
+  def part2(lines: List[String]): Int = solve(lines
+    .map { line =>
+      val hand = HandPart2(line.split(" ")(0).toList)
+      val bid = line.split(" ")(1).toInt
+      (hand, bid)
+    })
 }
--- src/test/scala/day07/Day07Test.scala
+++ src/test/scala/day07/Day07Test.scala
@@ -10,27 +10,27 @@ import scala.jdk.StreamConverters.*
 
 class Day07Test extends AnyWordSpec with Matchers {
 
-  "hand" should {
+  "HandPart1" should {
     "AAAAA is five of a kind" in {
-      Day07.Hand("AAAAA".toList).handType() should equal(Day07.HandType.FiveOfAKind)
+      Day07.HandPart1("AAAAA".toList).handType() should equal(Day07.HandType.FiveOfAKind)
     }
     "AA8AA is four of a kind" in {
-      Day07.Hand("AA8AA".toList).handType() should equal(Day07.HandType.FourOfAKind)
+      Day07.HandPart1("AA8AA".toList).handType() should equal(Day07.HandType.FourOfAKind)
     }
     "23332 is full house" in {
-      Day07.Hand("23332".toList).handType() should equal(Day07.HandType.FullHouse)
+      Day07.HandPart1("23332".toList).handType() should equal(Day07.HandType.FullHouse)
     }
     "TTT98 is three of a kind" in {
-      Day07.Hand("TTT98".toList).handType() should equal(Day07.HandType.ThreeOfAKind)
+      Day07.HandPart1("TTT98".toList).handType() should equal(Day07.HandType.ThreeOfAKind)
     }
     "23432 is two pair" in {
-      Day07.Hand("23432".toList).handType() should equal(Day07.HandType.TwoPairs)
+      Day07.HandPart1("23432".toList).handType() should equal(Day07.HandType.TwoPairs)
     }
     "A23A4 is one pair" in {
-      Day07.Hand("A23A4".toList).handType() should equal(Day07.HandType.OnePair)
+      Day07.HandPart1("A23A4".toList).handType() should equal(Day07.HandType.OnePair)
     }
     "23456 is high card" in {
-      Day07.Hand("23456".toList).handType() should equal(Day07.HandType.HighCard)
+      Day07.HandPart1("23456".toList).handType() should equal(Day07.HandType.HighCard)
     }
   }
 
@@ -47,16 +47,69 @@ class Day07Test extends AnyWordSpec with Matchers {
     }
   }
 
+
+  "HandPart2" should {
+    "AAAAA is five of a kind" in {
+      Day07.HandPart2("AAAAA".toList).handType() should equal(Day07.HandType.FiveOfAKind)
+    }
+    "AA8AA is four of a kind" in {
+      Day07.HandPart2("AA8AA".toList).handType() should equal(Day07.HandType.FourOfAKind)
+    }
+    "T55J5 is four of a kind" in {
+      Day07.HandPart2("T55J5".toList).handType() should equal(Day07.HandType.FourOfAKind)
+    }
+    "QQQJA is four of a kind" in {
+      Day07.HandPart2("QQQJA".toList).handType() should equal(Day07.HandType.FourOfAKind)
+    }
+    "KTJJT is four of a kind" in {
+      Day07.HandPart2("KTJJT".toList).handType() should equal(Day07.HandType.FourOfAKind)
+    }
+    "23332 is full house" in {
+      Day07.HandPart2("23332".toList).handType() should equal(Day07.HandType.FullHouse)
+    }
+    "TTT98 is three of a kind" in {
+      Day07.HandPart2("TTT98".toList).handType() should equal(Day07.HandType.ThreeOfAKind)
+    }
+    "23432 is two pair" in {
+      Day07.HandPart2("23432".toList).handType() should equal(Day07.HandType.TwoPairs)
+    }
+    "KK677 is two pair" in {
+      Day07.HandPart2("KK677".toList).handType() should equal(Day07.HandType.TwoPairs)
+    }
+    "A23A4 is one pair" in {
+      Day07.HandPart2("A23A4".toList).handType() should equal(Day07.HandType.OnePair)
+    }
+    "23456 is high card" in {
+      Day07.HandPart2("23456".toList).handType() should equal(Day07.HandType.HighCard)
+    }
+    "FullHouse with 1 joker" in {
+      Day07.HandPart2("22J33".toList).handType() should equal(Day07.HandType.FullHouse)
+    }
+    "OnePair with 1 joker" in {
+      Day07.HandPart2("2J345".toList).handType() should equal(Day07.HandType.OnePair)
+    }
+    "FiveOfAKind with 5 joker" in {
+      Day07.HandPart2("JJJJJ".toList).handType() should equal(Day07.HandType.FiveOfAKind)
+    }
+    "FiveOfAKind with 4 joker" in {
+      Day07.HandPart2("JJJJ1".toList).handType() should equal(Day07.HandType.FiveOfAKind)
+    }
+    "FourOfAKind with 3 joker" in {
+      Day07.HandPart2("JJJ12".toList).handType() should equal(Day07.HandType.FourOfAKind)
+    }
+  }
+
+
   "part2" should {
 
     "work for the sample" in {
       val measures = Files.lines(Paths.get("src/test/resources/day07/sample.txt")).toScala(List)
-      Day07.part2(measures) should equal(-1)
+      Day07.part2(measures) should equal(5905)
     }
 
     "work for the input" in {
       val measures = Files.lines(Paths.get("src/test/resources/day07/input.txt")).toScala(List)
-      Day07.part2(measures) should equal(-1)
+      Day07.part2(measures) should equal(252127335)
     }
   }
 }
