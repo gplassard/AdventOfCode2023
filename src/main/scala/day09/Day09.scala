bf13abfcda35541ca8746ae545de300e0634e0e1
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-09 15:24:16 +0100

    feat(day9): part1

---
+++ src/main/scala/day09/Day09.scala
@@ -0,0 +1,17 @@
+package fr.gplassard.adventofcode
+package day09
+
+object Day09 {
+  def part1(lines: List[String]): Int =
+    lines.map(line => solveSerie(line.split(" ").map(_.trim.toInt).toList)).sum
+
+  def solveSerie(serie: List[Int]): Int = {
+    if (serie.forall(_ == 0)) return 0
+    val derivative = serie.tail.zip(serie).map(t => t._1 - t._2)
+    serie.last + solveSerie(derivative)
+  }
+
+  def part2(lines: List[String]): Int = {
+    -1
+  }
+}
---
+++ src/test/scala/day09/Day09Test.scala
@@ -0,0 +1,53 @@
+package fr.gplassard.adventofcode
+package day09
+
+import org.scalatest.*
+import org.scalatest.matchers.should.*
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.nio.file.{Files, Paths}
+import scala.jdk.StreamConverters.*
+
+class Day09Test extends AnyWordSpec with Matchers {
+
+  "solve serie" should {
+
+    "handle the base case" in {
+      Day09.solveSerie(List(0,0,0,0,0)) should equal(0)
+    }
+
+    "handle the recursive case" in {
+      Day09.solveSerie(List(1,1,1,1,1,1)) should equal(1)
+    }
+
+    "handle the recursive case (part 2)" in {
+      Day09.solveSerie(List(2,3,4,5,6)) should equal(7)
+    }
+  }
+
+  "part1" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day09/sample.txt")).toScala(List)
+      Day09.part1(measures) should equal(114)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day09/input.txt")).toScala(List)
+      Day09.part1(measures) should equal(1647269739)
+    }
+  }
+
+  "part2" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day09/sample.txt")).toScala(List)
+      Day09.part2(measures) should equal(-1)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day09/input.txt")).toScala(List)
+      Day09.part2(measures) should equal(-1)
+    }
+  }
+}
