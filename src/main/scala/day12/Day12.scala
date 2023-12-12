ccea46e68f2c285640deaf0983e9f3f2c86ccbaf
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-12 21:56:26 +0100

    feat(day12): part2

--- src/main/scala/day12/Day12.scala
+++ src/main/scala/day12/Day12.scala
@@ -1,46 +1,57 @@
 package fr.gplassard.adventofcode
 package day12
 
-import scala.collection.mutable.{ListBuffer => MList}
+import scala.collection.mutable.{Map => MMap}
 
 object Day12 {
   object Record {
     def parse(line: String): Record = Record(line.split(" ")(0).toList, line.split(" ")(1).split(",").map(_.toInt).toList)
   }
   case class Record(springs: List[Char], damagedRecords: List[Int]) {
-    def isValid(): Boolean = {
-      val counters = MList.empty[Int]
-      var counter = 0
-      for {spring <- springs} {
-        if (spring == '#') {
-          counter = counter + 1
+    def count(cache: MMap[Record, BigInt] = MMap.empty): BigInt = {
+      if (cache.contains(this)) return cache(this)
+      if (damagedRecords.isEmpty && springs.isEmpty) return 0
+      if (damagedRecords.isEmpty) return if (springs.contains('#')) 0 else 1
+      if (springs.isEmpty && damagedRecords.nonEmpty) return 0
+      if (springs.head == '.') return this.copy(springs = this.springs.dropWhile(_ == '.')).count(cache)
+      if (springs.head == '#') {
+        val damagedCount = damagedRecords(0)
+        val nextDamaged = springs.take(damagedCount).filter(_ != '.') // same as transforming all possible unknown into damaged
+        if (nextDamaged.size != damagedCount) {
+          return 0
         }
-        else if (counter > 0) {
-          counters.addOne(counter)
-          counter = 0
+        if (damagedCount == springs.size) {
+          return if (damagedRecords.size == 1) 1 else 0
         }
+        val nextSprings = springs.drop(damagedCount)
+        if (nextSprings.head == '#') {
+          return 0
+        }
+        return this.copy(springs = nextSprings.updated(0, '.'), damagedRecords = this.damagedRecords.drop(1)).count(cache)
       }
-      if (counter > 0) {
-        counters.addOne(counter)
-      }
-      counters.toList == damagedRecords
-    }
-
-    def generate(index: Int): LazyList[Record] = {
-      if (index == springs.length) return LazyList(this)
-      if (springs(index) != '?') return generate(index + 1)
-      val left = this.copy(springs = this.springs.updated(index, '#')).generate(index + 1)
-      val right = this.copy(springs = this.springs.updated(index, '.')).generate(index + 1)
-      left ++ right
+      val left = this.copy(springs = springs.updated(0, '.'))
+      val right = this.copy(springs = springs.updated(0, '#'))
+      val leftCount = left.count(cache)
+      cache(left) = leftCount
+      val rightCount = right.count(cache)
+      cache(right) = rightCount
+      val sum = leftCount + rightCount
+      cache(this) = sum
+      sum
     }
   }
 
-  def part1(lines: List[String]): Int = {
+  def part1(lines: List[String]): BigInt = {
     val records = lines.map(Record.parse)
-    records.flatMap(_.generate(0).filter(_.isValid())).size
+    records.map(_.count()).sum
   }
 
-  def part2(lines: List[String]): Int = {
-    -1
+  def part2(lines: List[String]): BigInt = {
+    val records = lines.map(Record.parse).map(r => r.copy(
+      springs = r.springs ++ List('?') ++ r.springs ++ List('?') ++ r.springs ++ List('?') ++ r.springs ++ List('?') ++ r.springs,
+      damagedRecords = r.damagedRecords ++ r.damagedRecords ++ r.damagedRecords ++ r.damagedRecords ++ r.damagedRecords
+    ))
+
+    records.map(_.count()).sum
   }
 }
--- src/test/scala/day12/Day12Test.scala
+++ src/test/scala/day12/Day12Test.scala
@@ -11,11 +11,62 @@ import scala.jdk.StreamConverters.*
 class Day12Test extends AnyWordSpec with Matchers {
 
   "record" should {
-    "detect valid" in {
-      Day12.Record("#.#.###".toList, List(1,1,3)).isValid() should equal(true)
+    "count both empty" in {
+      Day12.Record(List.empty, List.empty).count() should equal(0)
     }
-    "detect invalid" in {
-      Day12.Record("#.#.###".toList, List(1, 2, 3)).isValid() should equal(false)
+    "count remaining errors counts" in {
+      Day12.Record(List.empty, List(1)).count() should equal(0)
+    }
+    "count remaining errors" in {
+      Day12.Record("####".toList, List(0)).count() should equal(0)
+    }
+    "count remaining solution is valid" in {
+      Day12.Record("........".toList, List.empty).count() should equal(1)
+    }
+    "count remaining solution is valid with errors" in {
+      Day12.Record("##..#..###..".toList, List(2,1,3)).count() should equal(1)
+    }
+    "count remaining solution with unknown" in {
+      Day12.Record("?#..#..###..".toList, List(2,1,3)).count() should equal(1)
+    }
+    "count remaining solution with more unknown" in {
+      Day12.Record("?#?.#..###..".toList, List(2,1,3)).count() should equal(2)
+    }
+    "count from sample" in {
+      Day12.Record("#.#.###".toList, List(1,1,3)).count() should equal(1)
+    }
+    "count from sample - 2.1" in {
+      Day12.Record("???.###".toList, List(1,1,3)).count() should equal(1)
+    }
+    "count from sample - 2.2" in {
+      Day12.Record(".??..??...?##.".toList, List(1,1,3)).count() should equal(4)
+    }
+    "count from sample - 2.3" in {
+      Day12.Record("?#?#?#?#?#?#?#?".toList, List(1,3, 1, 6)).count() should equal(1)
+    }
+    "count from sample - 2.4" in {
+      Day12.Record("????.#...#...".toList, List(4,1,1)).count() should equal(1)
+    }
+    "count from sample - 2.5" in {
+      Day12.Record("????.######..#####.".toList, List(1,6,5)).count() should equal(4)
+    }
+    "count from sample - 2.6" in {
+      Day12.Record("?###????????".toList, List(3,2,1)).count() should equal(10)
+    }
+    "count from sample - 2.6 - sub 1" in {
+      Day12.Record(".??????".toList, List(2,1)).count() should equal(6)
+    }
+    "count from sample - 2.6 - sub 2" in {
+      Day12.Record(".?????".toList, List(2,1)).count() should equal(3)
+    }
+    "count from sample - 2.6 - sub 3" in {
+      Day12.Record(".????".toList, List(2,1)).count() should equal(1)
+    }
+    "count from sample - 2.6 - sub 4" in {
+      Day12.Record(".???".toList, List(2,1)).count() should equal(0)
+    }
+    "count from sample - 2.6 - sub 5" in {
+      Day12.Record("#?".toList, List(2,1)).count() should equal(0)
     }
   }
 
@@ -40,13 +91,37 @@ class Day12Test extends AnyWordSpec with Matchers {
   "part2" should {
 
     "work for the sample" in {
-      val measures = Files.lines(Paths.get("src/test/resources/day12/sample.txt")).toScala(List)
-      Day12.part2(measures) should equal(-1)
+      val measures = Files.lines(Paths.get("src/test/resources/day12/sample-2.txt")).toScala(List)
+      Day12.part2(measures) should equal(525152)
+    }
+
+    "work for the sample - part 1" in {
+      Day12.part2(List("???.### 1,1,3")) should equal(1)
+    }
+
+    "work for the sample - part 2" in {
+      Day12.part2(List(".??..??...?##. 1,1,3")) should equal(16384)
+    }
+
+    "work for the sample - part 3" in {
+      Day12.part2(List("?#?#?#?#?#?#?#? 1,3,1,6")) should equal(1)
+    }
+
+    "work for the sample - part 4" in {
+      Day12.part2(List("????.#...#... 4,1,1")) should equal(16)
+    }
+
+    "work for the sample - part 5" in {
+      Day12.part2(List("????.######..#####. 1,6,5")) should equal(2500)
+    }
+
+    "work for the sample - part 6" in {
+      Day12.part2(List("?###???????? 3,2,1")) should equal(506250)
     }
 
     "work for the input" in {
       val measures = Files.lines(Paths.get("src/test/resources/day12/input.txt")).toScala(List)
-      Day12.part2(measures) should equal(-1)
+      Day12.part2(measures) should equal(BigInt(1537505634471L))
     }
   }
 }
