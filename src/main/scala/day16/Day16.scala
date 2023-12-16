5a69d97fbcadeb26614cf5975307eafb49ea02e0
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-12-16 19:07:42 +0100

    feat(day16): part1

---
+++ src/main/scala/day16/Day16.scala
@@ -0,0 +1,64 @@
+package fr.gplassard.adventofcode
+package day16
+
+import day16.Day16.Direction.*
+
+import scala.collection.mutable.{Set as MSet, Stack as MStack}
+
+object Day16 {
+  enum Direction(val deltaRow: Int, val deltaColumn: Int, val isHorizontal: Boolean) {
+    case LEFT extends Direction(0, -1, true)
+    case RIGHT extends Direction(0, 1, true)
+    case UP extends Direction(-1, 0, false)
+    case DOWN extends Direction(1, 0, false)
+  }
+  case class Beam(x: Int, y: Int, direction: Direction)
+
+  def part1(lines: List[String]): Int = {
+    val grid = lines.map(_.toList)
+    val beams = MSet.empty[Beam]
+    val toContinue = MStack(Beam(0, -1, RIGHT))
+    while (toContinue.nonEmpty) {
+      var beam: Option[Beam] = Some(toContinue.pop())
+      while (beam.isDefined && !beams.contains(beam.get)) {
+        beams += beam.get
+        val (nextBeam, eventualNewBeam) = next(beam.get, grid)
+        beam = nextBeam
+        eventualNewBeam.foreach(toContinue += _)
+      }
+    }
+    val energized = beams.map(b => (b.x, b.y))
+    energized.size - 1
+  }
+
+  def next(beam: Beam, grid: List[List[Char]]): (Option[Beam], Option[Beam]) = {
+    val nextPosition = (beam.x + beam.direction.deltaRow, beam.y + beam.direction.deltaColumn)
+    if (!inBound(nextPosition, grid)) {
+      return (None, None)
+    }
+    val nextAt = grid(nextPosition._1)(nextPosition._2)
+    val (first, second) = nextAt match {
+      case '-' if !beam.direction.isHorizontal => (LEFT, Some(RIGHT))
+      case '|' if beam.direction.isHorizontal => (UP, Some(DOWN))
+      case '/' if beam.direction == LEFT => (DOWN, None)
+      case '/' if beam.direction == RIGHT => (UP, None)
+      case '/' if beam.direction == UP => (RIGHT, None)
+      case '/' if beam.direction == DOWN => (LEFT, None)
+      case '\\' if beam.direction == LEFT => (UP, None)
+      case '\\' if beam.direction == RIGHT => (DOWN, None)
+      case '\\' if beam.direction == UP => (LEFT, None)
+      case '\\' if beam.direction == DOWN => (RIGHT, None)
+      case _ => (beam.direction, None)
+    }
+    (
+      Some(Beam(nextPosition._1, nextPosition._2, first)),
+      second.map(s => Beam(nextPosition._1, nextPosition._2, s))
+    )
+  }
+
+  def inBound(tuple: (Int, Int), grid: List[List[Char]]): Boolean = tuple._1 >= 0 && tuple._2 >= 0 && tuple._1 < grid.size && tuple._2 < grid.head.size
+
+  def part2(lines: List[String]): Int = {
+    -1
+  }
+}
---
+++ src/test/scala/day16/Day16Test.scala
@@ -0,0 +1,38 @@
+package fr.gplassard.adventofcode
+package day16
+
+import org.scalatest.*
+import org.scalatest.matchers.should.*
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.nio.file.{Files, Paths}
+import scala.jdk.StreamConverters.*
+
+class Day16Test extends AnyWordSpec with Matchers {
+
+  "part1" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day16/sample.txt")).toScala(List)
+      Day16.part1(measures) should equal(46)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day16/input.txt")).toScala(List)
+      Day16.part1(measures) should equal(7034)
+    }
+  }
+
+  "part2" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day16/sample.txt")).toScala(List)
+      Day16.part2(measures) should equal(-1)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day16/input.txt")).toScala(List)
+      Day16.part2(measures) should equal(-1)
+    }
+  }
+}
