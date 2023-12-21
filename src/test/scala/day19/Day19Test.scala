0931395ad1eb38ef0fa2737ffc1c6dc9d7971bcc
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-12-21 22:57:50 +0100

    feat(day19): part 1 (#4)

---
+++ src/test/scala/day19/Day19Test.scala
@@ -0,0 +1,38 @@
+package fr.gplassard.adventofcode
+package day19
+
+import org.scalatest.*
+import org.scalatest.matchers.should.*
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.nio.file.{Files, Paths}
+import scala.jdk.StreamConverters.*
+
+class Day19Test extends AnyWordSpec with Matchers {
+
+  "part1" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day19/sample.txt")).toScala(List)
+      Day19.part1(measures) should equal(19114)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day19/input.txt")).toScala(List)
+      Day19.part1(measures) should equal(489392)
+    }
+  }
+
+  "part2" should {
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day19/sample.txt")).toScala(List)
+      Day19.part2(measures) should equal(-1)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day19/input.txt")).toScala(List)
+      Day19.part2(measures) should equal(-1)
+    }
+  }
+}
